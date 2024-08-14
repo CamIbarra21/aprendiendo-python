f1afa97c50bf677b7fa4c7f694ac5a09272fe2df
Camila Ibarra <126617555+CamIbarra21@users.noreply.github.com>
2024-08-14 00:35:38 -0500

    finish archive for-foreach-while.py

--- 2.repetitivas/for-foreach-while.py
+++ 2.repetitivas/for-foreach-while.py
@@ -1,3 +1,4 @@
+import random
 
 # F O R   E A C H
 """
@@ -29,3 +30,24 @@ print("")
 
 for i in range(rango_min, rango_max, salto):
     print(f'Prueba con salto {i}')
+
+#WHILE
+"""
+Es una estructura repetitiva que requiere de una condicional para detenerse
+"""
+contador = 0
+while contador < 10:
+    contador+=1
+    print(f'Contador en {contador}')
+
+print("")
+
+numCorrecto = False
+aleatorio = random.randrange(0,100)
+
+while not numCorrecto:
+    numero = int(input('Ingrese un numero entero entre el 0 y el 100: '))
+    if numero < aleatorio: print("-> El numero que ingresaste es menor")
+    elif numero > aleatorio: print("-> El numero que ingresaste es mayor")
+    else: numCorrecto = True
+print(f'\n* * * Hallaste el numero aleatorio {aleatorio} * * *')
