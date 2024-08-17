18ba35da50b4b6ffb4b1c504ade65e43510ff722
Camila Ibarra <126617555+CamIbarra21@users.noreply.github.com>
2024-08-17 01:37:44 -0500

    finish arreglos.py archive

---
+++ 4.arrays-matrix/arreglos.py
@@ -0,0 +1,18 @@
+arregloEnteros = [1,4,2,3,6,5,7,8]
+print(arregloEnteros)
+
+#agregar nuevo elemento
+arregloEnteros.append(-5)
+print(arregloEnteros)
+
+#quitar elemento
+arregloEnteros.remove(3)
+print(arregloEnteros)
+
+#revertir el arreglo
+arregloEnteros.reverse()
+print(arregloEnteros)
+
+#ordenar los elementos del arreglo
+arregloEnteros.sort()
+print(arregloEnteros)
